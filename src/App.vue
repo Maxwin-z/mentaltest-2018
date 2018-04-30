46d07782a3c3e8664f5571f812e101a5aad8b8cd
Maxwin-z <zwd2005@gmail.com>
2018-04-30 16:45:39 +0800

    vue counter

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>Hi: {{message}}</div>
+  <div>Hi: {{message}}
+    <button v-on:click="add">+</button>
+    {{count}}
+    <button v-on:click="minus">-</button>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: 'hello vue'
+      message: 'hello vue',
+      count: 0
+    }
+  },
+  methods: {
+    add() {
+      ++this.count
+    },
+    minus() {
+      --this.count
     }
   }
 }
